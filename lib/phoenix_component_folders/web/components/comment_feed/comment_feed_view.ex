3960fc08b6f6158154cd32c0920da4a7d1c904d0
ksl <misc@dynevision.com>
2017-09-26 14:26:40 +0700

    Migrate example components to the new 1.3 directory structure

--- lib/phoenix_component_folders/web/components/comment_feed/comment_feed_view.ex
+++
@@ -1,4 +0,0 @@
-defmodule PhoenixComponentFolders.Web.Components.CommentFeedView do
-  alias PhoenixComponentFolders.Web.Components.ComponentHelpers
-  use PhoenixComponentFolders.Web, {:view, ComponentHelpers.view_opts(:comment_feed)}
-end
---
+++ lib/phoenix_component_folders_web/components/comment_feed/comment_feed_view.ex
@@ -0,0 +1,4 @@
+defmodule PhoenixComponentFoldersWeb.Components.CommentFeedView do
+  alias PhoenixComponentFoldersWeb.Components.ComponentHelpers
+  use PhoenixComponentFoldersWeb, {:view, ComponentHelpers.view_opts(:comment_feed)}
+end
