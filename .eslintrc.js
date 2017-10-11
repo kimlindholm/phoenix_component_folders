40c3a351bebaee065e244f84e42173af0f90dfe8
ksl <misc@dynevision.com>
2017-10-11 13:51:44 +0700

    Edit ESLint config: Double quotes, multiline comma-dangle, React support

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,32 +1,38 @@
-/*eslint-env node*/
 module.exports = {
+  env: {
+    browser: true,
+    es6: true
+  },
+  extends: "eslint:recommended",
   parserOptions: {
-    ecmaVersion: 6,
+    ecmaFeatures: {
+      experimentalObjectRestSpread: true,
+      jsx: true
+    },
     sourceType: "module"
   },
-  env: {
-    browser: true
-  },
+  plugins: ["react"],
   rules: {
-    camelcase: 2,
-    "comma-dangle": [2, "never"],
-    "comma-style": [2, "last"],
-    eqeqeq: 2,
-    indent: [2, 2, { VariableDeclarator: 2 }],
-    "no-eq-null": 2,
-    "no-extra-parens": 2,
-    "no-extra-semi": 2,
-    "no-lonely-if": 2,
+    camelcase: "error",
+    "comma-dangle": ["error", "only-multiline"],
+    "comma-style": ["error", "last"],
+    eqeqeq: "error",
+    indent: ["error", 2, { VariableDeclarator: 2 }],
+    "linebreak-style": ["error", "unix"],
+    "no-eq-null": "error",
+    "no-extra-parens": "error",
+    "no-extra-semi": "error",
+    "no-lonely-if": "error",
     "no-multi-spaces": 0,
-    "no-nested-ternary": 2,
-    "no-param-reassign": 2,
-    "no-self-compare": 2,
-    "no-shadow": 2,
-    "no-throw-literal": 2,
-    "no-undef": 2,
+    "no-nested-ternary": "error",
+    "no-param-reassign": "error",
+    "no-self-compare": "error",
+    "no-shadow": "error",
+    "no-throw-literal": "error",
+    "no-undef": "error",
     "no-underscore-dangle": 0,
-    "no-void": 2,
-    quotes: [2, "single"],
-    semi: [2, "always"]
+    "no-void": "error",
+    quotes: ["error", "double"],
+    semi: ["error", "always"]
   }
 };
